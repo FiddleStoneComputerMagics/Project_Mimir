8138cc08df8f175d85c813d63d7646debb40c9a0
JoJo_89752 <58885715+FiddleStoneComputerMagics@users.noreply.github.com>
2020-06-08 12:05:08 +0300

    Update er_d.sql

--- db/er_d.sql
+++ db/er_d.sql
@@ -1,70 +1,69 @@
-CREATE TABLE [faculty] (
-  [id] int PRIMARY KEY NOT NULL,
-  [name] nvarchar(255) NOT NULL,
-  [decan] nvarchar(255) NOT NULL
+CREATE TABLE faculty (
+  id int PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  dean varchar(255) NOT NULL
 );
 
-CREATE TABLE [departament] (
-  [id] int PRIMARY KEY NOT NULL,
-  [name_department] nvarchar(255) NOT NULL,
-  [list_workers] int NOT NULL
+CREATE TABLE departament (
+  id int PRIMARY KEY NOT NULL,
+  name_departament varchar(255) NOT NULL,
+  list_workers int NOT NULL
 );
 
-CREATE TABLE [building] (
-  [id] int PRIMARY KEY NOT NULL,
-  [name_korpus] nvarchar(255) NOT NULL,
-  [adress] nvarchar(255) NOT NULL,
-  [watchman] nvarchar(255) NOT NULL
+CREATE TABLE building (
+  id int PRIMARY KEY NOT NULL,
+  name_building varchar(255) NOT NULL,
+  adress varchar(255) NOT NULL,
+  watchmen varchar(255) NOT NULL
 );
 
-CREATE TABLE [room] (
-  [id] int PRIMARY KEY NOT NULL,
-  [nomber_room] int NOT NULL,
-  [floor] int NOT NULL,
-  [type_room] nvarchar(255) NOT NULL
+CREATE TABLE room (
+  id int PRIMARY KEY NOT NULL,
+  number int NOT NULL,
+  floor int NOT NULL,
+  type varchar(255) NOT NULL
 );
 
-CREATE TABLE [inf_room] (
-  [id] int PRIMARY KEY NOT NULL,
-  [length] float NOT NULL,
-  [width] float NOT NULL,
-  [height] float NOT NULL,
-  [volume] float NOT NULL,
-  [floor] float NOT NULL
+CREATE TABLE inf_room (
+  id int PRIMARY KEY NOT NULL,
+  length float NOT NULL,
+  width float NOT NULL,
+  height float NOT NULL,
+  volume float NOT NULL,
+  floor int NOT NULL
 );
 
-CREATE TABLE [list_workers] (
-  [id] int PRIMARY KEY NOT NULL,
-  [first_name] nvarchar(255) NOT NULL,
-  [second_name] nvarchar(255) NOT NULL,
-  [middle_name] nvarchar(255) NOT NULL,
-  [post] nvarchar(255) NOT NULL
+CREATE TABLE list_workers (
+  id int PRIMARY KEY NOT NULL,
+  first_name varchar(255) NOT NULL,
+  second_name varchar(255) NOT NULL,
+  post varchar(255) NOT NULL
 );
 
-CREATE TABLE [audit] (
-  [id] int PRIMARY KEY NOT NULL,
-  [name_audit] nvarchar(255) NOT NULL,
-  [data_audit] datatime NOT NULL,
-  [mark_audit] nvarchar(255) NOT NULL,
-  [recommendations] nvarchar(255)
+CREATE TABLE audit (
+  id int PRIMARY KEY NOT NULL,
+  name_audit varchar(255) NOT NULL,
+  data_audit atatime NOT NULL,
+  mark_audit varchar(255) NOT NULL,
+  recommendations varchar(255)
 );
 
-ALTER TABLE [departament] ADD FOREIGN KEY ([id]) REFERENCES [faculty] ([name]);
+ALTER TABLE departament ADD FOREIGN KEY (id) REFERENCES faculty (name);
 
-ALTER TABLE [building] ADD FOREIGN KEY ([id]) REFERENCES [departament] ([name_department]);
+ALTER TABLE building ADD FOREIGN KEY (id) REFERENCES departament (name_departament);
 
-ALTER TABLE [room] ADD FOREIGN KEY ([id]) REFERENCES [building] ([name_building]);
+ALTER TABLE room ADD FOREIGN KEY (id) REFERENCES building (name_building);
 
-ALTER TABLE [inf_room] ADD FOREIGN KEY ([id]) REFERENCES [room] ([number]);
+ALTER TABLE inf_room ADD FOREIGN KEY (id) REFERENCES room (number);
 
-ALTER TABLE [list_workers] ADD FOREIGN KEY ([id]) REFERENCES [departament] ([list_workers]);
+ALTER TABLE list_workers ADD FOREIGN KEY (id) REFERENCES departament (list_workers);
 
-ALTER TABLE [audit] ADD FOREIGN KEY ([id]) REFERENCES [departament] ([name_department]);
+ALTER TABLE audit ADD FOREIGN KEY (id) REFERENCES departament (name_department);
 
-CREATE UNIQUE INDEX [faculty_index_0] ON [faculty] ("dean", "name");
+CREATE UNIQUE INDEX faculty_index_0 ON faculty ("dean", "name");
 
-CREATE UNIQUE INDEX [departament_index_1] ON [departament] ("name_department");
+CREATE UNIQUE INDEX departament_index_1 ON departament] ("name_department");
 
-CREATE UNIQUE INDEX [building_index_2] ON [building] ("watchman", "adress");
+CREATE UNIQUE INDEX building_index_2 ON [building] ("watchmen", "adress");
 
-CREATE UNIQUE INDEX [room_index_3] ON [room] ("number");
+CREATE UNIQUE INDEX room_index_3 ON room ("number");
